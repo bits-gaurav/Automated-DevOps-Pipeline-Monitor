4be3cf2a42cf604cd1178eea31677859396ad5c5
gauravgidye26 <gauravgidye@benchmarkit.solutions>
2025-09-07 21:08:19 +0530

    chore: remove test text from index response message

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.get("/")
 async def index():
-    return PlainTextResponse("Hello from demo app! Test")
+    return PlainTextResponse("Hello from demo app!")
 
 
 @app.get("/health")
