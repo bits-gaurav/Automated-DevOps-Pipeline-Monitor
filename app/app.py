532a4ea47a0e3d8ff3a356002b9b2e1582b4b8e7
gauravgidye26 <gauravgidye@benchmarkit.solutions>
2025-09-07 19:57:36 +0530

    changed message

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.get("/")
 async def index():
-    return PlainTextResponse("Hello from demo app!")
+    return PlainTextResponse("Hello from demo app! Test")
 
 
 @app.get("/health")
